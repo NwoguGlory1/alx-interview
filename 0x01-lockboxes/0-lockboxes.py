f583958a281688a31ee959d8256615de5fe39c89
NwoguGlory1 <glorynwogu18@gmail.com>
2024-05-03 04:28:23 +0000

    Lockboxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-""" Lockboxes task"""
+
+""" Lockboxes """
 
 
 def canUnlockAll(boxes):
@@ -17,21 +18,17 @@ def canUnlockAll(boxes):
 
         n_keys = len(keys)
 
-        for i inrange(len(boxes)):
+        for i in range(len(boxes)):
             if boxes[i] and keys.get(i, False):
                 for j in boxes[i]:
                     if j < n_boxes:
                         keys[j] = True
                     boxes[i] = None
 
-                    if not (len(keys) > n_keys):
-                        break
+        if not(len(keys) > n_keys):
+            break
 
-                     if not(len(keys) > n_keys):
-                         break
-                     if n_keys == len(boxes):
-                         canUnlockAll = True
+    if n_keys == len(boxes):
+        canUnlockAll = True
 
-
-                    return canUnlockAll
-
+    return canUnlockAll
